268a5a782185ded58adb67d585ccda1fa535f1f7
Samuel Felton <78095426+SamFeltip@users.noreply.github.com>
2023-07-27 15:44:36 +0100

    mobile view and font awesome imported

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,53 @@
+import React from "react"
+import Sidebar from './Sidebar';
 
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+function Header() {
+    return (
+        <div className="text-red-100 break-words font-nunito px-[50px] 2xl:ps-[250px] pt-[10vh] lg:pt-[20vh] me-[70px] lg:me-[450px]">
+            <div className="pb-10">
+                <h1 className="text-[40px] lg:text-[50px] text-[#020b10ff] font-[600]">
+                    React Playground
+                </h1>
+                <h2 className="text-[30px] lg:text-[40px] text-[#020b10ff]">
+                    built by Sam Felton
+                </h2>
+                <h3 className="">
+                    <a className="text-[18px] lg:text-[28px] text-[#020b10ff] hover:text-[#0097ee]" href="https://about.samfelton.com">about.samfelton.com</a>
+                </h3>
+            </div>
+            <div className="pb-7">
+                <h2 className="text-[30px] lg:text-[40px] text-[#020b10ff]">
+                    About
+                </h2>
+                <p className="text-[18px] lg:text-[28px] text-[#020b10ff]">
+                    This page shows off some example pages for when I was learning how to use React
+                </p>
+            </div>
+            <div className='flex flex-row flex-wrap gap-5 justify-center'>
+                <img src="/logos/JavaScript.png" alt="JavaScript logo" className='h-[50px]'/>
+                <img src="/logos/Figma.png" alt="Figma logo" className='h-[50px]'/>
+                <img src="/logos/NodeJS.png" alt="NodeJS logo" className='h-[50px]'/>
+                <img src="/logos/React.png" alt="React Logo" className='h-[50px]'/>
+                <img src="/logos/Tailwind.png" alt="Tailwind Logo" className='h-[50px]'/>
+            </div>
+        </div>
+    )
+}
 
 function App() {
-  const [data, setData] = React.useState(null);
 
-  React.useEffect(() => {
-    fetch("/api")
-        .then((res) => res.json())
-        .then((data) => setData(data.message));
-  }, []);
+    const sidebar_items = [
+        "Introduction",
+        "classes",
+        "something else"
+    ]
 
-  return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{!data ? "Loading..." : data}</p>
-        </header>
-      </div>
-  );
+    return (
+        <div>
+            <Sidebar items={sidebar_items}/>
+            <Header/>
+        </div>
+    )
 }
 
 export default App;
---
+++ client/src/Sidebar.js
@@ -0,0 +1,63 @@
+import React, {useState} from 'react';
+
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {icon} from "@fortawesome/fontawesome-svg-core/import.macro";
+
+import {Routes, Route} from "react-router-dom";
+
+function SidebarItem({description, index}) {
+    return (
+        <div className='block lg:table-row hover:text-black cursor-pointer pb-10'>
+            <div className='lg:pr-10 block lg:table-cell text-center'>
+                <p>Chapter {index+1}</p>
+            </div>
+            <div className='block lg:table-cell text-center lg:text-end'>
+                <p>{description}</p>
+            </div>
+        </div>
+    )
+}
+
+function Sidebar({items}) {
+    const [open, setOpen] = useState(false)
+    let body = document.getElementById("body");
+
+    const toggleOpen = () => {
+        setOpen(!open)
+        console.log(open)
+
+        body.classList.toggle('dark');
+
+    }
+
+    const table_rows = items.map((item, index) => {
+        return <SidebarItem description={item} index={index}/>
+    })
+    let menuIcon = open ? 'bars' : "x"
+    return (
+        <div className={"fixed right-0 bg-[#0097ee] text-white font-nunito h-screen lg:w-[450px] linear-in duration-200 " + (open ? "w-[80%]" : "w-[80px]")}>
+
+            <button className={"absolute right-[30px] top-[30px] lg:hidden font-[900]"} onClick={toggleOpen}>
+                <FontAwesomeIcon icon={open ? icon({name: "x" }) : icon({name: "bars" }) } size= "xl" />
+            </button>
+
+            <div className={"pt-[20vh] mx-5 " + (open ? "block" : "hidden lg:block")}>
+
+                <h1 className="text-[40px] lg:text-[50px] text-white text-center">
+                    Sections
+                </h1>
+
+
+                <div className="text-[28px] flex flex-row justify-center items-center gap-[35px] box-border">
+                    <div>
+                        {table_rows}
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar;
+
